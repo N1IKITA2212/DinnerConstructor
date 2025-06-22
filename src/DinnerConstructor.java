5cee2b83b06b286ae46e6b3ad9c0fd4f49161a5d
Nikita German <N1IKITA23@yandex.ru>
2025-06-22 20:11:35 +0300

    first commit

---
+++ src/DinnerConstructor.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Set;
+
+public class DinnerConstructor {
+    private HashMap<String, ArrayList<String>> dishes = new HashMap<>();
+    Random random = new Random();
+
+    public void addDish(String typeOfDish, String dish) {
+        if (!dishes.containsKey(typeOfDish)) {
+            dishes.put(typeOfDish, new ArrayList<>());
+        }
+        dishes.get(typeOfDish).add(dish);
+    }
+
+    public ArrayList<String> makeCombo(ArrayList<String> typesOfDishes) {
+        ArrayList<String> combo = new ArrayList<>();
+        for (String type : typesOfDishes) {
+            ArrayList<String> dishesByType = dishes.get(type);
+            combo.add(dishesByType.get(random.nextInt(dishesByType.size())));
+        }
+        return combo;
+    }
+
+    public void printDishes() {
+        for (String type : dishes.keySet()) {
+            System.out.print("В категории " + type + " имеются следующие блюда: " + dishes.get(type));
+        }
+        System.out.println();
+    }
+
+    public boolean hasType(String type) {
+        return dishes.containsKey(type);
+    }
+
+    public boolean isEmpty() {
+        return dishes.isEmpty();
+    }
+
+    public Set<String> getSet() {
+        return dishes.keySet();
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final DinnerConstructor dinnerConstructor = new DinnerConstructor();
+
+    public static void main(String[] args) {
+        printMenu();
+        int cmd = scanner.nextInt();
+        scanner.nextLine();
+        while (cmd != 4) {
+            if (cmd == 1) {
+                System.out.print("Введите тип блюда: ");
+                String type = scanner.nextLine();
+                System.out.print("Введите название блюда: ");
+                String dish = scanner.nextLine();
+                dinnerConstructor.addDish(type, dish);
+            } else if (cmd == 2) {
+                if (dinnerConstructor.isEmpty()) {
+                    System.out.println("Вы не ввели ни одного блюда");
+                    continue;
+                }
+                System.out.println("Начинаем создание комбинаций...");
+                System.out.print("Введите количество комбинаций: ");
+                int numberOfCombos = scanner.nextInt();
+                scanner.nextLine();
+                ArrayList<String> typesOfDishesForCombo = makeListOfTypes();
+                for (int i = 1; i <= numberOfCombos; i++) {
+                    System.out.println("Комбо " + i);
+                    System.out.println(dinnerConstructor.makeCombo(typesOfDishesForCombo));
+                }
+            } else if (cmd == 3) {
+                dinnerConstructor.printDishes();
+            }
+            else {
+                System.out.println("Такой команды нет, введите от 1 до 4");
+            }
+            printMenu();
+            cmd = scanner.nextInt();
+            scanner.nextLine();
+        }
+    }
+
+    public static void printMenu() {
+        System.out.println("-".repeat(20));
+        System.out.println("Выберите команду:");
+        System.out.println("1 - Добавить новое блюдо");
+        System.out.println("2 - Сгенерировать комбинации блюд");
+        System.out.println("3 - Вывести все введенные блюда");
+        System.out.println("4 - Выход");
+        System.out.println("-".repeat(20));
+    }
+
+    public static ArrayList<String> makeListOfTypes() {
+        ArrayList<String> typesOfDishesForCombo = new ArrayList<>();
+
+        System.out.println("Вводите типы блюд, каждый новый тип с новой строки. Для завершения ввода " +
+                "введите пустую строку.");
+        String typeOfDish = scanner.nextLine();
+        while (!typeOfDish.isEmpty()) {
+            if (dinnerConstructor.hasType(typeOfDish)) {
+                typesOfDishesForCombo.add(typeOfDish);
+            } else {
+                System.out.println("Такого типа блюд нет, введите одно из этих: " + dinnerConstructor.getSet());
+            }
+            typeOfDish = scanner.nextLine();
+        }
+        return typesOfDishesForCombo;
+    }
+
+
+}
